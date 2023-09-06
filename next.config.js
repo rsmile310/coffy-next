a75466468e99ed59af74df7822ec50d05638ddec
rsmile310 <78564494+rsmile310@users.noreply.github.com>
2023-09-06 02:44:17 -0700

    update package

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
-module.exports = {
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    domains: ["*"],
+    loader: "akamai",
+    path: "",
+    unoptimized: true,
+  },
+  NETLIFY_NEXT_PLUGIN_SKIP: true,
+  assetPrefix: "",
+  compiler: {
+    styledComponents: true,
+  },
 };
+
+module.exports = nextConfig;
