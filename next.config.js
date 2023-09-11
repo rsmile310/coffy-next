8527efc0ae24fd077d562d252830611990f2529b
rsmile310 <78564494+rsmile310@users.noreply.github.com>
2023-09-11 11:57:33 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 const nextConfig = {
   output: "export",
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
     return {
-      "/": { page: "/" },
-      "/qrscanner/anytoken": {
-        page: "/qrscanner/[slug]",
-        query: { slug: "anytoken" }
-      },
       ...defaultPathMap,
+      '/qrscanner/anytoken': { page: '/qrscanner/[slug]', query: { slug: 'anytoken' } },
     }
   },
   images: {
