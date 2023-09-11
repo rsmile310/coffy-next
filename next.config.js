9866a0a1e7eb9cd091f6387a32f8691586f5b12c
rsmile310 <78564494+rsmile310@users.noreply.github.com>
2023-09-11 11:23:45 -0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  target: "static",
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
